f9d00266170c7bf0df2284432039667c29e95d9d
idalmas <ian.dalmas@icloud.com>
2024-10-21 01:10:25 -0700

    please work (added boostrap button)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
+      
+      <button className="btn btn-primary btn-lg">
+        Join the Waitlist
+      </button>
+    </div>
+  );
 }
